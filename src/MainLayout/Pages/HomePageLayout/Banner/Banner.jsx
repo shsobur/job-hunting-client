625e79b1f4d9e83e16e94187289b1d1924458330
SOBUR HOSSEN <shsoburhossen961@gmail.com>
2025-08-31 12:00:29 +0600

    Backup befour reset

--- src/MainLayout/Pages/HomePageLayout/Banner/Banner.jsx
+++ src/MainLayout/Pages/HomePageLayout/Banner/Banner.jsx
@@ -1,6 +1,29 @@
+import "./Banner.css";
+
 const Banner = () => {
   return (
-    <div>This is Banner</div>
+    <>
+      <section id="main_banner_container">
+        <div className="banner_bg_image_container">
+          <div className="banner_overlay_container">
+
+            <div className="banner_content">
+              <h1>Find the job that fits you life</h1>
+              <p>Join over 10 million people finding careers with us. Explore opportunities that match you skill adn aspirations.</p>
+
+              <div className="jon_search_input_container">
+                <form>
+                  <input type="text" name="job" placeholder="🔍 Job title or keyword" />
+                  <button>Search</button>
+                </form>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import router from "./Routes/Routes.jsx";
 // Package__
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router/dom";
+import { RouterProvider } from "react-router-dom";
 
 
 createRoot(document.getElementById("root")).render(
