c428557113183bbe68f5c70fd61ed5062e24c6fc
SOBUR HOSSEN <shsoburhossen961@gmail.com>
2025-09-01 21:33:48 +0600

    Sign up step one design completed

--- src/MainLayout/Pages/Authentication/SignUp/SignUp.jsx
+++ src/MainLayout/Pages/Authentication/SignUp/SignUp.jsx
@@ -1,6 +1,8 @@
 import "./SignUp.css";
 import logo from "../../../../assets/companyLogo.png";
 import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { HiOutlineInformationCircle } from "react-icons/hi";
 
 const SignUp = () => {
   const [step, setStep] = useState(1);
@@ -18,8 +20,38 @@ const SignUp = () => {
                 className="slider"
                 style={{ transform: `translateX(-${(step - 1) * (100 / 4)}%)` }}
               >
-                <div className="step">This is content 1</div>
-                <div className="step">This is content 2</div>
+                <div className="step">
+                  <div className="step_one_content">
+                    <h1>How would you like to sign up?</h1>
+                    <button>
+                      <FcGoogle /> Sign In With Google
+                    </button>
+                    <p>
+                      <HiOutlineInformationCircle size={22} />{" "}
+                      <i>
+                        Sign up as a <b>user</b>(Job Seeker)
+                      </i>
+                    </p>
+                    <button onClick={handleNext}>
+                      Sign In With Email <p>(Recommended)</p>→
+                    </button>
+                    <p>
+                      <HiOutlineInformationCircle size={22} />{" "}
+                      <i>
+                        If you want to be a <b>recruiter</b>, <br /> sign up
+                        with email and choose your role
+                      </i>
+                    </p>
+                  </div>
+                </div>
+
+                <div className="step">
+                  <div className="step_two_content">
+                    
+                  </div>
+                </div>
+
+
                 <div className="step">This is content 3</div>
                 <div className="step">This is content 4</div>
               </div>
@@ -42,16 +74,17 @@ const SignUp = () => {
             {step === 4 ? (
               <button className="btn btn-accent">Submit</button>
             ) : (
-              <button
-                disabled={step === 4}
-                onClick={handleNext}
-                className="btn btn-accent"
-              >
-                Next
-              </button>
+              step > 1 && (
+                <button
+                  disabled={step === 4}
+                  onClick={handleNext}
+                  className="btn btn-accent"
+                >
+                  Next
+                </button>
+              )
             )}
           </div>
-          
         </div>
       </section>
     </>
