ae6c7f1b8e78b8ae99341e849f99931151a26eba
SOBUR HOSSEN <shsoburhossen961@gmail.com>
2025-09-01 20:16:57 +0600

    Sign up page basic design completed

--- src/MainLayout/Main.jsx
+++ src/MainLayout/Main.jsx
@@ -3,14 +3,21 @@ import Navbar from "./Shared/Navbar/Navbar";
 import Footer from "./Shared/Footer/Footer";
 
 // Package__
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import ProgressBar from "../Components/ProgressBar/ProgressBar";
 
 const Main = () => {
+  // eslint-disable-next-line no-unused-vars
+  const path = useLocation();
+
   return (
     <>
       <ProgressBar></ProgressBar>
+      {/* {
+        path.pathname !== "/sign-up" && <Navbar></Navbar>
+      } */}
       <Navbar></Navbar>
+      
       <Outlet></Outlet>
       <Footer></Footer>
     </>
--- src/MainLayout/Pages/Authentication/SignUp/SignUp.jsx
+++ src/MainLayout/Pages/Authentication/SignUp/SignUp.jsx
@@ -1,18 +1,61 @@
-import "../AuthStyle/AuthStyle.css";
+import "./SignUp.css";
+import logo from "../../../../assets/companyLogo.png";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [step, setStep] = useState(1);
+
+  const handleNext = () => setStep(step + 1);
+  const handleBack = () => setStep(step - 1);
 
   return (
     <>
-      <section>
-        <div>
-          <h1>
-            Hello this is sign up page!
-          </h1>
+      <section id="main_signUp_container">
+        <div className="signUp_inner_container">
+          <div className="signUp_parent_content_container">
+            <div className="signUp_content">
+              <div
+                className="slider"
+                style={{ transform: `translateX(-${(step - 1) * (100 / 4)}%)` }}
+              >
+                <div className="step">This is content 1</div>
+                <div className="step">This is content 2</div>
+                <div className="step">This is content 3</div>
+                <div className="step">This is content 4</div>
+              </div>
+            </div>
+
+            <div className="company_logo">
+              <img src={logo} alt="Company Logo" />
+            </div>
+          </div>
+
+          <div className="signUp_process_button">
+            <button
+              disabled={step === 1}
+              onClick={handleBack}
+              className="btn btn-accent"
+            >
+              Previous
+            </button>
+
+            {step === 4 ? (
+              <button className="btn btn-accent">Submit</button>
+            ) : (
+              <button
+                disabled={step === 4}
+                onClick={handleNext}
+                className="btn btn-accent"
+              >
+                Next
+              </button>
+            )}
+          </div>
+          
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
